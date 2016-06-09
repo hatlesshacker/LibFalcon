56ff2062614603d88a0eac5a025599598b689e59
Syed Nasim <warhacker.2002@gmail.com>
2016-06-09 17:47:39 +0530

    added function prototype for inportl and outportl. Updated CREDITS.

--- include/kernel.h
+++ include/kernel.h
@@ -1,7 +1,11 @@
-unsigned char inportb (unsigned short _port);
-uint16_t inportw (uint16_t port);
-void outportb (unsigned short _port, unsigned char _data);
-void outportw (unsigned short _port, uint16_t _data);
+#include <stdint.h>
+
+unsigned char inportb (unsigned short);
+uint16_t inportw (uint16_t);
+void outportb (unsigned short , unsigned char);
+void outportw (unsigned short , uint16_t);
+unsigned int inportl(unsigned short);
+void outportl(unsigned int, unsigned short);
 void reboot();
 char* get_cpuvendor();
 int __get_cpuvendor();
