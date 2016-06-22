4922994318203b323d209cca3b36d7689292696b
Syed Nasim <warhacker.2002@gmail.com>
2016-06-22 21:26:38 +0530

    Fixed `swab`.

--- include/stdint.h
+++ include/stdint.h
@@ -41,5 +41,6 @@ typedef unsigned char		uint8_t;
 typedef unsigned short int	uint16_t;
 typedef unsigned int		uint32_t;
 typedef unsigned long long int	uint64_t;
+typedef int ssize_t;
 
 #endif /* _STDINT_H */
