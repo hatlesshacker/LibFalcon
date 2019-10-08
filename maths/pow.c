512812c27c4fae8ab717834c13261adb9db80428
Guillem Ramírez Miranda <guillem.ramirez.miranda@est.fib.upc.edu>
2019-10-08 15:57:12 +0200

    "gcc-6" doesn't always work. Replaced it with "gcc" instead. Might cause some glitches.
    Signed-off-by: Syed Nasim <nasim@warmachine.rf.gd>

--- maths/pow.c
+++ maths/pow.c
@@ -1,23 +1,17 @@
 /* This File is Part of LibFalcon.
-
  * Copyright (c) 2018, Syed Nasim
    All rights reserved.
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
-
    * Redistributions of source code must retain the above copyright notice, this
      list of conditions and the following disclaimer.
-
    * Redistributions in binary form must reproduce the above copyright notice,
      this list of conditions and the following disclaimer in the documentation
      and/or other materials provided with the distribution.
-
    * Neither the name of LibFalcon nor the names of its
      contributors may be used to endorse or promote products derived from
      this software without specific prior written permission.
-
-HIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
@@ -27,28 +21,30 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 */
 
-#ifndef _MATHS_H
-#define _MATHS_H
-
-#ifdef __cplusplus
+#if defined(__cplusplus)
 extern "C" {
 #endif
 
+#include <ctype.h>
 #include <stdint.h>
-#include <stddef.h>
-
-int32_t add(const int32_t, const int32_t);
-int32_t div(const int32_t, const int32_t);
-int32_t mod(const int32_t, const int32_t);
-int32_t mul(const int32_t, const int32_t);
-int32_t sub(const int32_t, const int32_t);
-int32_t pow(const int32_t, const int32_t);
-
-#ifdef __cplusplus
+int32_t pow(const int32_t a, const int32_t b) {
+	int32_t result = 1;
+	int32_t base = a;
+	int32_t exp = b;
+	for (;;)
+	{
+		if (exp & 1)
+			result *= base;
+		exp >>= 1;
+		if(!exp)
+			break;
+		base *= base;
+	}
+	return result;
 }
-#endif
 
+#if defined(__cplusplus)
+}
 #endif
