46e4865175b951f8c5b0036e7aaf61c9ad720277
Syed Nasim <nasim@warmachine.rf.gd>
2020-01-17 16:47:44 +0530

    Added rand() stub

--- stdlib/rand.c
+++ stdlib/rand.c
@@ -30,30 +30,14 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#define max(a,b)        (((a) > (b)) ? (a) : (b))
-#define min(a,b)        (((a) < (b)) ? (a) : (b))
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
-#include <stdint.h>
-#include <stddef.h>
+int rand() {
+    return 0; //TODO
+}
 
-typedef struct {
-    int quot, rem;
-} div_t;
-typedef struct {
-    long quot, rem;
-} ldiv_t;
-typedef struct {
-    long long quot, rem;
-} lldiv_t;
-
-long atol(const char*);
-int atoi(const char*);
-int abs(int);
-void *bsearch(const void* , const void* , size_t, size_t , int (*cmp)(const void *, const void *));
-long long atoll(const char*);
-long long llabs(long long);
-long labs(long);
-lldiv_t lldiv(long long, long long);
-ldiv_t ldiv(long, long);
-div_t div(int32_t, int32_t);
-int rand();
+#if defined(__cplusplus)
+}
+#endif
